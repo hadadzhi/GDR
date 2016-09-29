8e46867e04c0b988af72274d853fda591315e894
hadadzhi <hadadzhi@gmail.com>
2016-09-29 19:11:09 +0300

    .

--- src/main/java/ru/cdfe/gdr/constants/Parameters.java
+++ src/main/java/ru/cdfe/gdr/constants/Parameters.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Parameters {
     public static final String ID = "exfor_subent_number";
-    
     public static final String ENERGY_COLUMN = "energy_column";
     public static final String CROSS_SECTION_COLUMN = "cross_section_column";
     public static final String CROSS_SECTION_ERROR_COLUMN = "cross_section_error_column";
