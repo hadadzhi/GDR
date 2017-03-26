ffd262bb800600f3f5bd86f14023a76b2f8ba9e5
hadadzhi <hadadzhi@gmail.com>
2017-03-26 19:04:44 +0300

    MOVED

--- src/main/java/ru/cdfe/gdr/exceptions/NoSuchRecordException.java
+++
@@ -1,7 +0,0 @@
-package ru.cdfe.gdr.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class NoSuchRecordException extends RuntimeException {}
--- src/main/java/ru/cdfe/gdr/repositories/RecordsRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.cdfe.gdr.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import ru.cdfe.gdr.domain.Record;
-
-public interface RecordsRepository extends PagingAndSortingRepository<Record, String> {}
